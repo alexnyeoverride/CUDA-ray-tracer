ce3c6fcffc9717978f36644f5dd3f0ce85e3b81c
alex <alexnye@outlook.com>
2025-03-12 17:32:01 -0700

    first sphere

--- vector.cuh
+++ vector.cuh
@@ -14,6 +14,10 @@ __device__ __host__ inline double4 operator*(const double4& a, const double& sca
 	return {a.x * scale, a.y * scale, a.z * scale, a.w * scale};
 }
 
+__device__ __host__ inline double dot(const double4& a, const double4& b) {
+	return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+}
+
 __device__ __host__ inline double length(const double4& a) {
 	return sqrt(pow(a.x, 2) + pow(a.y, 2) + pow(a.z, 2) + pow(a.w, 2));
 }
